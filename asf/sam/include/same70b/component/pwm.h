bd992f3f8b7b233689ec78f7877241889195c0d9
Stephanos Ioannidis <root@stephanos.io>
2020-03-16 10:38:31 +0900

    same70b: patch: Fix PWM_CH_NUM register name
    This commit patches the incorrect PWM_CH_NUM register name in the Atmel
    SAME70 DFP version 2.4.166.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- asf/sam/include/same70b/component/pwm.h
+++ asf/sam/include/same70b/component/pwm.h
@@ -2763,7 +2763,7 @@ typedef struct {
   __I  uint8_t                        Reserved6[68];
        PwmCmp   PWM_CMP[PWMCMP_NUMBER]; /**< Offset: 0x130 PWM Comparison 0 Value Register */
   __I  uint8_t                        Reserved7[80];
-       PwmChNum PwmChNum[PWMCHNUM_NUMBER]; /**< Offset: 0x200 PWM Channel Mode Register */
+       PwmChNum PWM_CH_NUM[PWMCHNUM_NUMBER]; /**< Offset: 0x200 PWM Channel Mode Register */
   __I  uint8_t                        Reserved8[384];
   __O  uint32_t PWM_CMUPD0;     /**< (PWM Offset: 0x400) PWM Channel Mode Update Register (ch_num = 0) */
   __I  uint8_t                        Reserved9[28];
@@ -2846,7 +2846,7 @@ typedef struct {
   __I  uint8_t                        Reserved6[68];
        PwmCmp                         PWM_CMP[8];      /**< Offset: 0x130 PWM Comparison 0 Value Register */
   __I  uint8_t                        Reserved7[80];
-       PwmChNum                       PwmChNum[4];    /**< Offset: 0x200 PWM Channel Mode Register */
+       PwmChNum                       PWM_CH_NUM[4];    /**< Offset: 0x200 PWM Channel Mode Register */
   __I  uint8_t                        Reserved8[384];
   __O  PWM_CMUPD0_Type                PWM_CMUPD0;     /**< Offset: 0x400 ( /W  32) PWM Channel Mode Update Register (ch_num = 0) */
   __I  uint8_t                        Reserved9[28];
